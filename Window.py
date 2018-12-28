7588852182d735128cc31ce7ce4f7f1eef8d4f46
Mario996 <klisanicmario@gmail.com>
2018-12-28 13:02:41 +0100

    added boundries to window

--- Window.py
+++ Window.py
@@ -2,14 +2,16 @@ import pygame
 
 pygame.init()
 
-x=50
-y=50
+xPlayerPosition= 16
+yPlayerPosition= 630
 playerWidth = 40
 playerHeight = 60
-vel = 15
+windowWidth = 900
+windowHeight = 700
+velocity = 15
 running = True
 
-window = pygame.display.set_mode((900, 700))
+window = pygame.display.set_mode((windowWidth, windowHeight))
 
 background_colour = (255, 255, 255)
 pygame.display.set_caption('Bubble trouble')
@@ -26,13 +28,13 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        x -= vel
-    if keys[pygame.K_RIGHT]:
-        x += vel
+    if keys[pygame.K_LEFT] and xPlayerPosition > velocity:
+        xPlayerPosition -= velocity
+    if keys[pygame.K_RIGHT] and xPlayerPosition < windowWidth - playerWidth - velocity:
+        xPlayerPosition += velocity
 
     window.fill((255, 255, 255))
-    pygame.draw.rect(window, (255, 0, 0), (x, y, playerWidth, playerHeight)) #za iscrtavanje naseg lika
+    pygame.draw.rect(window, (255, 0, 0), (xPlayerPosition, yPlayerPosition, playerWidth, playerHeight)) #za iscrtavanje naseg lika
     pygame.display.update()  #da bi se on pojavio na ekranu
 
 pygame.quit()
