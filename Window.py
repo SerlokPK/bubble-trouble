5a05746f72514b2533f980dba94e068779f12c9f
Mario Klisanic <klisanicmario@gmail.com>
2018-12-28 17:32:42 +0100

    Added projectile class

--- Window.py
+++ Window.py
@@ -2,6 +2,7 @@ import pygame
 
 from Player1 import *
 from Player2 import *
+from Projectile import *
 
 pygame.init()
 windowWidth = 900
@@ -13,12 +14,14 @@ window = pygame.display.set_mode((windowWidth, windowHeight))
 pygame.display.set_caption('Bubble trouble')
 player1 = Player1(16, 630, 'player.png')
 player2 = Player2(860, 630, 'player2.png')
+projectile = Projectile(player1)
 
 
 def redrawWindow():
     window.fill((255, 255, 255))
     window.blit(player1.image, (player1.xPosition, player1.yPosition))  #iscrtavanje naseg player1
     window.blit(player2.image, (player2.xPosition, player2.yPosition))  # iscrtavanje naseg player2
+    window.blit(projectile.image, (projectile.xPosition, projectile.yPosition))
     pygame.display.update()  # da bi se oni pojavili na ekranu
 
 
@@ -31,6 +34,7 @@ while running:
 
     UpdatePlayer1(player1)
     UpdatePlayer2(player2)
+    UpdateProjectile(projectile)
     redrawWindow()
 
 pygame.quit()
