0c27bcc0f8415b711de7c451e8c3720487f904cb
Mario996 <klisanicmario@gmail.com>
2019-01-03 16:19:05 +0100

    added shooting projectiles to the game

--- Projectile.py
+++ Projectile.py
@@ -3,20 +3,18 @@ import pygame
 class Projectile(pygame.sprite.Sprite):
     def __init__(self, Player1):
         self.image = pygame.image.load("arrow.png")
-        self.xPosition = Player1.xPosition
-        self.yPosition = Player1.yPosition
+        self.xPosition = Player1.xPosition + 6
+        self.yPosition = Player1.yPosition + 34
         self.velocity = 10
         self.alive = False
-        super().__init__()
+        self.rect = self.image.get_rect()
 
-def UpdateProjectile(self):
-    keys = pygame.key.get_pressed()
+    def UpdateProjectile(self):
 
-    if keys[pygame.K_SPACE]:  #ako je stisnut space
-        self.alive = True  #aktiviramo projektil
         if self.alive:
             if self.yPosition > self.velocity:
                 self.yPosition -= self.velocity
             else:
                 self.alive = False
-                self.kill()
+                self.xPosition = -20
+
--- Window.py
+++ Window.py
@@ -14,17 +14,16 @@ class Window:
 
         self.window = pygame.display.set_mode((self.windowWidth, self.windowHeight))
         pygame.display.set_caption('Bubble trouble')
-        self.player1 = Player(16, 630, 'player.png',pygame.K_a,pygame.K_d)
-        self.player2 = Player(860, 630, 'player2.png',pygame.K_LEFT,pygame.K_RIGHT)
-        self.projectile = Projectile(self.player1)
-
+        self.player1 = Player(16, 630, 'player.png', pygame.K_a, pygame.K_d)
+        self.player2 = Player(860, 630, 'player2.png', pygame.K_LEFT, pygame.K_RIGHT)
 
 
     def redrawWindow(self):
         self.window.fill((255, 255, 255))
+        self.window.blit(self.player1.projectile.image, (self.player1.projectile.xPosition, self.player1.projectile.yPosition))
+        self.window.blit(self.player2.projectile.image, (self.player2.projectile.xPosition, self.player2.projectile.yPosition))
         self.window.blit(self.player1.image, (self.player1.xPosition, self.player1.yPosition))  #iscrtavanje naseg player1
         self.window.blit(self.player2.image, (self.player2.xPosition, self.player2.yPosition))  # iscrtavanje naseg player2
-        self.window.blit(self.projectile.image, (self.projectile.xPosition, self.projectile.yPosition))
         pygame.display.update()  # da bi se oni pojavili na ekranu
 
 
@@ -35,10 +34,16 @@ class Window:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_SPACE]:
+                    self.player1.fire()
+                elif keys[pygame.K_KP_ENTER]:
+                    self.player2.fire()
 
             PlayerMovement.UpdatePlayer(self, self.player1)
             PlayerMovement.UpdatePlayer(self, self.player2)
-            UpdateProjectile(self.projectile)
+            Projectile.UpdateProjectile(self.player1.projectile)
+            Projectile.UpdateProjectile(self.player2.projectile)
             self.redrawWindow()
 
     pygame.quit()
