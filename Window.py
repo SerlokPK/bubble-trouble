54e47b4033eb62e6f8d9502868fbcf1760fc3e79
DaliborOletic <doletic1@gmail.com>
2019-01-03 13:58:23 +0100

    Merged bubble and player windows into one window. Ball flickering fixed.

--- Window.py
+++ Window.py
@@ -5,46 +5,48 @@ from playerMovement import PlayerMovement
 from Projectile import *
 from bubble import Bubble
 
+
 class Window:
-	def __init__(self):
-		pygame.init()
-		self.windowWidth = 900
-		self.windowHeight = 700
-		self.running = True
-		self.clock = pygame.time.Clock()
+    def __init__(self):
+        pygame.init()
+        self.windowWidth = 900
+        self.windowHeight = 700
+        self.running = True
+        self.clock = pygame.time.Clock()
 
-		self.window = pygame.display.set_mode((self.windowWidth, self.windowHeight))
-		pygame.display.set_caption('Bubble trouble')
-		self.player1 = Player(16, 630, 'player.png',pygame.K_a,pygame.K_d)
-		self.player2 = Player(860, 630, 'player2.png',pygame.K_LEFT,pygame.K_RIGHT)
-		self.projectile = Projectile(self.player1)
+        self.window = pygame.display.set_mode((self.windowWidth, self.windowHeight))
+        pygame.display.set_caption('Bubble trouble')
+        self.player1 = Player(16, 630, 'player.png', pygame.K_a, pygame.K_d)
+        self.player2 = Player(860, 630, 'player2.png', pygame.K_LEFT, pygame.K_RIGHT)
+        self.projectile = Projectile(self.player1)
 
-		self.bubbleSize = 81                                #size of bubble
-		self.positionOfBall = (400, 50)
-		self.bubble = Bubble(self.positionOfBall,self.window,self.running,(self.windowWidth, self.windowHeight),self.bubbleSize)      #check later, it will always be true (?)
+        self.bubbleSize = 81  # size of bubble
+        self.positionOfBall = (400, 50)
+        self.bubble = Bubble(self.positionOfBall, self.window, self.running, (self.windowWidth, self.windowHeight),
+                             self.bubbleSize)  # check later, it will always be true (?)
 
-	def redrawWindow(self):
-		self.window.fill((255, 255, 255))
-		self.window.blit(self.player1.image, (self.player1.xPosition, self.player1.yPosition))  #iscrtavanje naseg player1
-		self.window.blit(self.player2.image, (self.player2.xPosition, self.player2.yPosition))  # iscrtavanje naseg player2
-		self.window.blit(self.projectile.image, (self.projectile.xPosition, self.projectile.yPosition))
-		pygame.display.update()  # da bi se oni pojavili na ekranu
+    def redrawWindow(self):
+        self.window.fill((255, 255, 255))
+        self.window.blit(self.player1.image,
+                         (self.player1.xPosition, self.player1.yPosition))  # iscrtavanje naseg player1
+        self.window.blit(self.player2.image,
+                         (self.player2.xPosition, self.player2.yPosition))  # iscrtavanje naseg player2
+        self.window.blit(self.projectile.image, (self.projectile.xPosition, self.projectile.yPosition))
+        self.bubble.move_ball()
+        pygame.display.update()  # da bi se oni pojavili na ekranu
 
+    def runGame(self):
+        self.bubble.init_ball(1)
+        while self.running:
+            self.clock.tick(40)
 
-	def runPlayers(self):
-		while self.running:
-			self.clock.tick(40)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					self.running = False
+            PlayerMovement.UpdatePlayer(self, self.player1)
+            PlayerMovement.UpdatePlayer(self, self.player2)
+            UpdateProjectile(self.projectile)
+            self.redrawWindow()
 
-			PlayerMovement.UpdatePlayer(self, self.player1)
-			PlayerMovement.UpdatePlayer(self, self.player2)
-			UpdateProjectile(self.projectile)
-			self.redrawWindow()
-
-	def runBubble(self):
-		self.bubble.move_ball(1)
-
-	pygame.quit()
+    pygame.quit()
--- bubble.py
+++ bubble.py
@@ -14,26 +14,20 @@ class Bubble:
 		self.background_colour = (255, 255, 255)        #white color
 		self.img = pygame.image.load('rball6.bmp')
 		self.img = pygame.transform.scale(self.img, (self.bubble_size, self.bubble_size))
+		self.my_bubbles = []
 
-	def move_ball(self,numOfBubbles):
+	def init_ball(self, numOfBubbles):
 		number_of_bubbles = numOfBubbles
-		my_bubbles = []
 
 		for n in range(number_of_bubbles):
-			bubble = BubbleMovement(self.positionOfBall, self.screen,(self.width,self.height), self.bubble_size)
-			bubble.speed = 0.8
+			bubble = BubbleMovement(self.positionOfBall, self.screen, (self.width, self.height), self.bubble_size)
+			bubble.speed = 15.8
 			bubble.angle = 2
 
-			my_bubbles.append(bubble)
+			self.my_bubbles.append(bubble)
 
-		while self.running:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					self.running = False
-
-			self.screen.fill(self.background_colour)
-
-			for bubble in my_bubbles:
+	def move_ball(self):
+			for bubble in self.my_bubbles:
 				bubble.move()
 				bubble.bounce()
 				bubble.display(self.img)
