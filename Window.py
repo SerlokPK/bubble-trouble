ab2f62131c70e4a9684ddb78aa4498ffd5771297
Mario996 <klisanicmario@gmail.com>
2019-01-05 14:51:26 +0100

    added player collision

--- Window.py
+++ Window.py
@@ -5,6 +5,7 @@ from level import Level
 from playerMovement import PlayerMovement
 from Projectile import *
 from bubble import Bubble
+from level import *
 
 class Window:
 	def __init__(self):
@@ -13,6 +14,7 @@ class Window:
 		self.windowHeight = 700
 		self.running = True
 		self.clock = pygame.time.Clock()
+		self.players = []
 
 		self.window = pygame.display.set_mode((self.windowWidth, self.windowHeight))
 		pygame.display.set_caption('Bubble trouble')
@@ -34,13 +36,31 @@ class Window:
 		self.window.blit(self.player1.image, (self.player1.xPosition, self.player1.yPosition))  # iscrtavanje naseg player1
 		self.window.blit(self.player2.image, (self.player2.xPosition, self.player2.yPosition))  # iscrtavanje naseg player2
 		self.bubble.move_ball()
+		self.updatehitboxes()
+		pygame.draw.rect(self.window, (255, 0, 0), self.bubble.my_bubbles[0].hitbox, 2)
+		pygame.draw.rect(self.window, (255,0,0), self.player1.hitbox, 2)
+		pygame.draw.rect(self.window, (255,0,0), self.player2.hitbox, 2)
 		pygame.display.update()  # da bi se oni pojavili na ekranu
 
+	def updatehitboxes(self):
+		self.player1.hitbox = (self.player1.xPosition, self.player1.yPosition, 23, 37)  #updating the hitboxes as players move
+		self.player2.hitbox = (self.player2.xPosition, self.player2.yPosition, 23, 37)
+		self.bubble.my_bubbles[0].hitbox = (self.bubble.my_bubbles[0].x, self.bubble.my_bubbles[0].y, 80, 80)  #we can use for loop to update all bubbles
+
+	def playerandballcollision(self):
+		for player in self.players:
+			for bubble in self.bubble.my_bubbles:
+				if bubble.y - 40 < player.hitbox[1] + player.hitbox[3] and bubble.y + 40 > player.hitbox[1] - player.hitbox[3]:
+					if bubble.x + 40 > player.hitbox[0] - player.hitbox[2] and bubble.x - 40 < player.hitbox[0] + player.hitbox[2]:
+						self.level.restart_level(self.player1, self.player2, self.bubble)
 
 	def runGame(self):
 		self.bubble.init_ball(1)
+		self.players.append(self.player1)
+		self.players.append(self.player2)
 		while self.running:
 			self.clock.tick(40)
+			self.playerandballcollision()
 
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
--- bubble_movement.py
+++ bubble_movement.py
@@ -1,36 +1,36 @@
 import pygame
 import math
 
-
-class BubbleMovement:
-	def __init__(self, position, screen, window_size, bubble_size):
+class BubbleMovement():
+	def __init__(self, position,screen,window_size,bubble_size):
 		self.x, self.y = position
-		self.width, self.height = window_size
+		self.width,self.height = window_size
 		self.bubble_size = bubble_size
 		self.screen = screen
 		self.speed = 0
 		self.angle = 0
 		self.firstTouch = True
+		self.img = pygame.image.load('Images/transparentBall.png')
+		self.hitbox = (self.x, self.y, 74, 74)
 
-	def display(self, image):
-		self.screen.blit(image, (self.x, self.y))
+	def display(self,image):
+		self.screen.blit(image,(self.x,self.y))
 
 	def move(self):
 		self.x += (math.sin(self.angle) * self.speed) / 8
 		self.y -= (math.cos(self.angle) * self.speed)
+
+
 
 	def bounce(self):
 		if self.x > self.width - self.bubble_size:
 			self.x = 2 * (self.width - self.bubble_size) - self.x
 			self.angle = - self.angle
 		elif self.x < 1:
-			if self.x < 0:
-				self.x * 2 * (-self.x)
-			else:
-				self.x = 2 * self.x
+			self.x = 2 * self.x
 			self.angle = - self.angle
 
-		if self.firstTouch is False and self.y <= self.height / 3 :
+		if self.firstTouch == False and self.y <= self.height / 3 :
 			self.y = 3*(self.height / 3 - self.bubble_size) - self.y
 			self.angle = math.pi - self.angle
 
