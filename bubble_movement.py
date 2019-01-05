1d3e1908e84fcef367535b77518c65765c27e922
DaliborOletic <doletic1@gmail.com>
2019-01-05 12:09:35 +0100

    When speed is increased, left wall bouncing fixed. Ball no longer gets out of the window.

--- bubble_movement.py
+++ bubble_movement.py
@@ -2,34 +2,35 @@ import pygame
 import math
 
 
-class BubbleMovement():
-	def __init__(self, position,screen,window_size,bubble_size):
+class BubbleMovement:
+	def __init__(self, position, screen, window_size, bubble_size):
 		self.x, self.y = position
-		self.width,self.height = window_size
+		self.width, self.height = window_size
 		self.bubble_size = bubble_size
 		self.screen = screen
 		self.speed = 0
 		self.angle = 0
 		self.firstTouch = True
 
-	def display(self,image):
-		self.screen.blit(image,(self.x,self.y))
+	def display(self, image):
+		self.screen.blit(image, (self.x, self.y))
 
 	def move(self):
 		self.x += (math.sin(self.angle) * self.speed) / 8
 		self.y -= (math.cos(self.angle) * self.speed)
-
-
 
 	def bounce(self):
 		if self.x > self.width - self.bubble_size:
 			self.x = 2 * (self.width - self.bubble_size) - self.x
 			self.angle = - self.angle
 		elif self.x < 1:
-			self.x = 2 * self.x
+			if self.x < 0:
+				self.x * 2 * (-self.x)
+			else:
+				self.x = 2 * self.x
 			self.angle = - self.angle
 
-		if self.firstTouch == False and self.y <= self.height / 3 :
+		if self.firstTouch is False and self.y <= self.height / 3 :
 			self.y = 3*(self.height / 3 - self.bubble_size) - self.y
 			self.angle = math.pi - self.angle
 
